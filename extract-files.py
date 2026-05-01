f4d86f726a4bb38a902f632eafc4b59d157dc0ee
Jose Miranda <josemiranda51263@gmail.com>
2026-05-01 22:11:14 +0000

    Revert "duchamp: Drop duplicated camera libraries"
    This reverts commit dc464148cdca0a92c5d7266ade4a26f1499af95d.

--- extract-files.py
+++ extract-files.py
@@ -30,7 +30,9 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    ('vendor.mediatek.hardware.apuware.utils-V1-ndk',
+    ('libmialgo_aio_seg',
+     'libmialgo_utils',
+     'vendor.mediatek.hardware.apuware.utils-V1-ndk',
      'vendor.mediatek.hardware.apuware.utils@2.0',
      'vendor.mediatek.hardware.videotelephony-V1-ndk',): lib_fixup_vendor_suffix,
 }
